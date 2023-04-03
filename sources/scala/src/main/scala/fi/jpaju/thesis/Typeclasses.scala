ce0de74fba8fc561c419cc30851527e3288202d0
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2023-04-03 15:49:43 +0300

    Small improvements and fixes

--- sources/scala/src/main/scala/fi/jpaju/thesis/Typeclasses.scala
+++ sources/scala/src/main/scala/fi/jpaju/thesis/Typeclasses.scala
@@ -1,45 +1,19 @@
 package fi.jpaju.thesis
 
-object Show:
+trait Ordering[A]:
+  extension (lhs: A) def <(rhs: A): Boolean
 
-  // Define type class
-  trait Show[A]:
-    extension (a: A) def show: String
+case class Money(amount: Int)
 
-  case class Person(age: Int, name: String)
+given Ordering[Money] with
+  extension (self: Money) def <(that: Money): Boolean =
+    self.amount < that.amount
 
-  // Define type class instance
-  given Show[Person] with
-    extension (a: Person) def show: String = s"${a.name} is ${a.age} years old"
+// Compatible with any data type that has instance for Ordering
+def sort[A: Ordering](as: List[A]): List[A] = as.sortWith(_ < _)
 
-  // Use the type class
-  def showAll[A: Show](as: List[A]): List[String] =
-    as.map(a => a.show)
+val sorted = sort(List(Money(3), Money(1), Money(2)))
 
-end Show
-
-object Ordering:
-
-  case class Money(amount: Int)
-
-  trait Ordering[A]:
-    extension (lhs: A) def <(rhs: A): Boolean
-
-  def sort[A: Ordering](as: List[A]): List[A] = as.sortWith(_ < _)
-
-  given Ordering[Money] with
-    extension (self: Money) def <(that: Money): Boolean = self.amount < that.amount
-
-  val sorted = sort(List(Money(3), Money(1), Money(2)))
-
-end Ordering
-
-import Ordering.*
-import Show.*
 
 @main def orderingMain: Unit =
   println(sorted)
-
-@main def showMain: Unit =
-  val persons = List(Person(20, "John"), Person(30, "Mary"), Person(40, "Bob"), Person(50, "Alice"))
-  println(showAll(persons))
